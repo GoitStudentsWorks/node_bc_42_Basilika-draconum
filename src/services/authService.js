f71a6a49485b28b93ec485d9ed00418827c47d89
Osyfliak Ivan <osuflak22@gmail.com>
2023-04-27 18:01:38 +0300

    1

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,25 @@
-// export const registerUserApi = async user => {
-//   const { data } = await publicAPI.post('auth/register', user);
-//   return data;
-// };
+import { privateAPI, publicAPI } from 'http/http';
+
+export const token = {
+  set: token => {
+    privateAPI.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unSet: () => {
+    privateAPI.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const registerService = async credentials => {
+  const { data } = await publicAPI.post('register', credentials);
+  return data;
+};
+
+export const loginService = async credentials => {
+  const { data } = await publicAPI.post('login', credentials);
+  return data;
+};
+
+export const logoutService = async () => {
+  const { data } = await privateAPI.post('logout');
+  return data;
+};
