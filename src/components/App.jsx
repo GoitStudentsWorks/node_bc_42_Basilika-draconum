69fb325e09e25c9731c3c72e121ea9545be458dd
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-04-28 18:48:21 +0300

    Update App.jsx
    222

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { RegisterPage } from '../pages/AuthPage/RegisterPage';
 import { LoginPage } from 'pages/AuthPage/LoginPage';
+import { StartPage } from 'pages/StartPage/MainPage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<StartPage />} />
     </Routes>
   );
 };
