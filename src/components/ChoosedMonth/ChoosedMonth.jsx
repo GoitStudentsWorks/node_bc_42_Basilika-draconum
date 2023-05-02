d6443b2e2c8d33a0f183d437a1a6f0881adf10d6
Lika <14dubinenkoanzhelika@gmail.com>
2023-05-02 13:14:08 +0300

    fix definition

--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { isWithinInterval } from 'date-fns';
 import { useDaysOfMonth } from 'hooks/useDaysOfMonth';
 import { selectDate } from 'redux/date/dateSelectors';
-import { selectTasks } from 'redux/tasks/tasksSelectors';
+import { selectArrTasks } from 'redux/tasks/tasksSelectors';
 import MonthCalendarHead from './MonthCalendarHead/MonthCalendarHead';
 import CalendarTable from './CalendarTable/CalendarTable';
 import css from './choosed-month.module.scss';
@@ -10,7 +10,7 @@ import css from './choosed-month.module.scss';
 const ChoosedMonth = () => {
   const currentDate = useSelector(selectDate);
 
-  const tasks = useSelector(selectTasks);
+  const tasks = useSelector(selectArrTasks);
 
   const { startMonth, endMonth } = useDaysOfMonth(currentDate);
 
