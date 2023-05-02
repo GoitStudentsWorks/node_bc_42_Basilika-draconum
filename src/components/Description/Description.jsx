c15c131518fb857bb59bb5b1b1d54398b1dff5d4
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-05-02 13:01:52 +0300

    edit description
    edit description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-// import mobcalendar from '../../images/mobcalendar.png'
-// import one from '../../images/mobone.png'
-// import sidebar from '../../images/mobsidebar.png'
-// import style from '../Description/description.module.css'
 import style from './description.module.scss'
 
 export const Description = () => {
@@ -12,16 +8,17 @@ export const Description = () => {
         <section className={style.section}>
           <div className={style.part}>
             <div className={style.partWrapper}>
-              <h2 className={style.number}>1.</h2>
-              <h3 className={style.title}>CALENDAR</h3>
-              <h4 className={style.subtitle}>VIEW</h4>
-              <p className={style.descript}>
-                GooseTrack's Calendar view provides a comprehensive overview of
-                your schedule, displaying all your tasks, events, and
-                appointments in a visually appealing and intuitive layout.
-              </p>
+              <div className={style.block}>
+                <h2 className={style.number}>1.</h2>
+                <h3 className={style.title}>CALENDAR</h3>
+                <h4 className={style.subtitle}>VIEW</h4>
+                <p className={style.descript}>
+                  GooseTrack's Calendar view provides a comprehensive overview
+                  of your schedule, displaying all your tasks, events, and
+                  appointments in a visually appealing and intuitive layout.
+                </p>
+              </div>
             </div>
-            {/* <img src={calendar} alt="" /> */}
             <picture>
               <source
                 srcset={require('images/deskcalendar.png')}
@@ -46,20 +43,22 @@ export const Description = () => {
           </div>
         </section>
 
-        <section>
-          <div className={style.part}>
-            <div className={style.sidebarWrapper}>
-              <h2 className={style.number}>2.</h2>
-              <h4 className={style.subtitle}>SIDEBAR</h4>
-              <p className={style.descript}>
-                GooseTrack offers easy access to your account settings,
-                calendar, and filters. The "My Account" section allows you to
-                manage your profile information and preferences, while the
-                calendar provides a quick and convenient way to view your
-                upcoming events and tasks.
-              </p>
+        <section className={style.section}>
+          <div className={`${style.part} ${style.partEven}`}>
+            <div className={style.partWrapper}>
+              <div className={style.block}>
+                <h2 className={style.number}>2.</h2>
+                <h4 className={`${style.subtitle} ${style.subtitleEven}`}>SIDEBAR</h4>
+                <p className={style.descript}>
+                  GooseTrack offers easy access to your account settings,
+                  calendar, and filters. The "My Account" section allows you to
+                  manage your profile information and preferences, while the
+                  calendar provides a quick and convenient way to view your
+                  upcoming events and tasks.
+                </p>
+              </div>
             </div>
-            {/* <img src={sidebar} alt="" /> */}
+           
             <picture>
               <source
                 srcset={require('images/desksaidbar.png')}
@@ -81,38 +80,39 @@ export const Description = () => {
           </div>
         </section>
 
-        <section>
-          <div className={style.part}>
-            <div className={style.partWrapper}>
-              <h2 className={style.number}>3.</h2>
-              <h3 className={style.title}>All in</h3>
-              <h4 className={style.subtitle}>ONE</h4>
-              <p className={style.descript}>
-                GooseTrack is an all-in-one productivity tool that helps you
-                stay on top of your tasks, events, and deadlines. Say goodbye to
-                scattered to-do lists and hello to streamlined productivity with
-                GooseTrack.
-              </p>
+        <section className={style.section}>
+          <div className={style.partWrapper}>
+            <div className={style.part}>
+              <div className={style.block}>
+                <h2 className={style.number}>3.</h2>
+                <h3 className={style.title}>All in</h3>
+                <h4 className={style.subtitle}>ONE</h4>
+                <p className={style.descript}>
+                  GooseTrack is an all-in-one productivity tool that helps you
+                  stay on top of your tasks, events, and deadlines. Say goodbye
+                  to scattered to-do lists and hello to streamlined productivity
+                  with GooseTrack.
+                </p>
+              </div>
+              <picture>
+                <source
+                  srcset={require('images/deskall.png')}
+                  media="(min-width: 1440px)"
+                  type="image/png"
+                />
+                <source
+                  srcset={require('images/tabone.png')}
+                  media="(min-width: 768px)"
+                  type="image/png"
+                />
+                <source
+                  srcset={require('images/mobone.png')}
+                  media="(max-width: 767px)"
+                  type="image/png"
+                />
+                <img src={require('images/mobone.png')} alt="foto one" />
+              </picture>
             </div>
-            {/* <img src={one} alt="" /> */}
-            <picture>
-              <source
-                srcset={require('images/deskall.png')}
-                media="(min-width: 1440px)"
-                type="image/png"
-              />
-              <source
-                srcset={require('images/tabone.png')}
-                media="(min-width: 768px)"
-                type="image/png"
-              />
-              <source
-                srcset={require('images/mobone.png')}
-                media="(max-width: 767px)"
-                type="image/png"
-              />
-              <img src={require('images/mobone.png')} alt="foto one" />
-            </picture>
           </div>
         </section>
       </div>
