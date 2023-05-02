bd1d7f94220a52a5e52ce438f19ee8c09a582838
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-05-02 11:27:46 +0300

    100% ready

--- src/redux/tasks/tasksOperations.js
+++ src/redux/tasks/tasksOperations.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getTaskAllApi } from '../../services/tasksService';
+import { getTaskAllApi, deleteTaskApi, updateTaskStatusApi } from '../../services/tasksService';
 
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchTasks',
@@ -10,6 +10,32 @@ export const fetchTasks = createAsyncThunk(
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteTaskThunk = createAsyncThunk(
+  'task/delete',
+  async (credentials, thunkAPI) => {
+    try {
+      console.log('deleteTaskApi');
+      const data = await deleteTaskApi(credentials);      
+      return data;
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateTaskStatusThunk = createAsyncThunk(
+  'task/updateStatus',
+  async (credentials, thunkAPI) => {
+    try {
+      //console.log('updateStatus');
+      const data = await updateTaskStatusApi(credentials);      
+      return data;
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.message);
     }
   }
 );
