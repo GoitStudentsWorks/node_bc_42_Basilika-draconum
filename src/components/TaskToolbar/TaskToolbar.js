960b99f4f798174d9b07e01f13e17a927e578549
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-05-04 15:44:22 +0300

    розмітка TaskToolbar

--- src/components/TaskToolbar/TaskToolbar.js
+++ src/components/TaskToolbar/TaskToolbar.js
@@ -2,20 +2,30 @@ import css from './TaskToolbar.module.css';
 import {  useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'; 
 import {TaskConfirmationModal} from './TaskConfirmationModal';
-import { TaskForm } from '../TaskModal/TaskForm';
+import TaskPopUp from '../TaskModal/TaskForm';
 import { isModalEditShownAction } from 'redux/tasks/tasksSlice';
 import { selectIsModalEditShown } from 'redux/tasks/tasksSelectors';
 import { updateTaskStatusThunk } from 'redux/tasks/tasksOperations';
 import icon from '../../images/icons.svg';
 import Notiflix from 'notiflix';
   
-export const TaskToolbar = ({ task }) => {   
+export const TaskToolbar = ({ task }) => { 
+    console.log(task);  
     const [isModalStatus, setIsModalStatus] = useState(false);
      
     const [isModalConfirmation, setIsModalConfirmation] = useState(false);   
     const dispatch = useDispatch(); 
-    const isModalEdit = useSelector(selectIsModalEditShown);   
+    const isModalEdit = useSelector(selectIsModalEditShown);       
     const statusArray = ['To do', 'In progress', 'Done'];
+    let currentStatus = task.status;
+    if (currentStatus === 'toDo'){
+      currentStatus = 'To do';
+    }else{
+      if (currentStatus === 'inProgress'){
+        currentStatus = 'In progress';
+      }
+    }
+
   
                     
     const toggleModal = () => {     
@@ -24,7 +34,9 @@ export const TaskToolbar = ({ task }) => {
       setIsModalStatus(prev => !prev);   
   };
        
-    const openTaskModal = id => {     
+    const openTaskModal = id => { 
+      setIsModalConfirmation(false);
+      setIsModalStatus(false);     
       dispatch(isModalEditShownAction(!isModalEdit)); 
     };
   
@@ -52,7 +64,7 @@ export const TaskToolbar = ({ task }) => {
           {isModalStatus && (
             <div className={css.statusModal} data-modal="true">
               {statusArray
-                .filter(status => status !== task.status)
+                .filter(status => status !== currentStatus)
                 .map(status => (
                   <button className={css.statusModal__btn}
                     key={status}
@@ -74,7 +86,7 @@ export const TaskToolbar = ({ task }) => {
               <use xlinkHref={icon + '#icon-pencil'}></use>
             </svg>
           </button> 
-          {isModalEdit && <TaskForm task={task}/>}
+          {isModalEdit && <TaskPopUp task={task}/>}
 
           <button className={css.toolbar__btn} onClick={confirmationModalOpen}>
             <svg className={css.toolbar__svg}>
