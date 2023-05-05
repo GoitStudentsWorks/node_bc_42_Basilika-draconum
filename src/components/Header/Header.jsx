9856f0b993f16d8b7f3708192807b69ebd4c8e0a
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-05-05 19:18:43 +0300

    add header goose

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,13 @@ import { useSelector } from 'react-redux';
 import { getUser } from 'redux/auth/authSelectors';
 import { getUserAvatar } from './../../redux/auth/authSelectors';
 import ThemeToggler from 'components/ThemeToggler/ThemeToggler';
-
+import goose from '../../images/goose-header.png';
+import { useParams } from 'react-router-dom';
 const Header = ({ setMenuActive }) => {
   const userName = useSelector(getUser);
   const userAvatar = useSelector(getUserAvatar);
-  //console.log(userAvatar);
+  const params = useParams();
+  const avatarName = userName.name.slice(0, 1).toUpperCase();
 
   return (
     <header className={s.header}>
@@ -21,17 +23,40 @@ const Header = ({ setMenuActive }) => {
             <use href={`${icon}#icon-burgerMenu`}></use>
           </svg>
         </button>
+        <div className={s.header__boxPage}>
+          {params.currentDay && (
+            <img src={goose} alt="Goose" width={64} height={60} />
+          )}
+          <div className={s.header__boxPage__description}>
+            <p className={s.header__boxPage__titlePage}>
+              {params.currentDate || params.currentDay
+                ? 'Calendar'
+                : 'User Profile'}
+            </p>
+            {params.currentDay && (
+              <p className={s.header__boxPage__message}>
+                <span className={s.header__boxPage__message__textBlue}>
+                  Let go
+                </span>{' '}
+                of the past and focus on the present!
+              </p>
+            )}
+          </div>
+        </div>
         <div className={s.boxContent}>
-          {/* <div className={s.boxContent__thema}></div> */}
           <ThemeToggler />
 
           <div className={s.boxContent__name}>{userName.name}</div>
           <div className={s.boxContent__avatar}>
-            <img
-              src={userAvatar}
-              alt="Avatar"
-              className={s.boxContent__avatar__img}
-            />
+            {userAvatar ? (
+              <img
+                src={userAvatar}
+                alt="Avatar"
+                className={s.boxContent__avatar__img}
+              />
+            ) : (
+              <p className={s.boxContent__avatar__title}>{avatarName}</p>
+            )}
           </div>
         </div>
       </div>
