8f3e7e90f4f0f4658f45446d52cc4c5b031f5ba2
Mariia Kryzhalko <kruzhalko93@gmail.com>
2023-05-06 17:02:27 +0300

    min-fixed

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -4,16 +4,15 @@ import CalendarToolbar from 'components/CalendarToolbar/CalendarToolbar';
 import css from './calendar-page.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from 'redux/tasks/tasksOperations';
-import { getUser } from 'redux/auth/authSelectors';
+import { getAccessToken } from 'redux/auth/authSelectors';
 
 const CalendarPage = () => {
   const dispatch = useDispatch();
-  const isUser = useSelector(getUser);
+  const token = useSelector(getAccessToken);
 
   useEffect(() => {
-    if (!isUser) return;
-    dispatch(fetchTasks());
-  }, [dispatch, isUser]);
+     token &&  dispatch(fetchTasks());
+  }, [dispatch, token]);
 
   return (
     <div className={css.calendarPageContainer}>
